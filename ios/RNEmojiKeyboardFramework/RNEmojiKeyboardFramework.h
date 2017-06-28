10b14bf5e8f6c2e35c3ea1225c407440b618d55d
Brendan Rius <brendan.rius@gmail.com>
2017-06-28 21:06:19 +0200

    test

---
+++ ios/RNEmojiKeyboardFramework/RNEmojiKeyboardFramework.h
@@ -0,0 +1,19 @@
+//
+//  RNEmojiKeyboardFramework.h
+//  RNEmojiKeyboardFramework
+//
+//  Created by Brendan Rius on 6/28/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNEmojiKeyboardFramework.
+FOUNDATION_EXPORT double RNEmojiKeyboardFrameworkVersionNumber;
+
+//! Project version string for RNEmojiKeyboardFramework.
+FOUNDATION_EXPORT const unsigned char RNEmojiKeyboardFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNEmojiKeyboardFramework/PublicHeader.h>
+
+
